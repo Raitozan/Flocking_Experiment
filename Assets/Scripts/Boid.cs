f884a2597df6377bee56d3c015d69ba06f4aa528
Nathan Desages <nathan.desages@gmail.com>
2018-10-08 20:02:49 +0200

    Initialisation projet boid seek

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float maxForce;
+	float maxSpeed;
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+
+		Vector3 direction = new Vector3 (Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+		direction = direction.normalized * maxSpeed;
+
+		rb.AddForce (direction);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		seek ();
+		updateStats ();
+	}
+
+	void seek() {
+		Vector2 desired = target.position - transform.position;
+		desired = desired.normalized * maxSpeed;
+
+		Vector2 steer = desired - rb.velocity;
+		steer = steer.normalized * maxForce;
+
+		rb.AddForce (steer);
+	}
+
+	void updateStats() {
+		maxForce = GameManager.instance.maxForce;
+		maxSpeed = GameManager.instance.maxSpeed;
+	}
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public Boid boidPrefab;
+	public List<Boid> boids;
+	public float maxForce;
+	public float maxSpeed;
+
+	public GameObject player;
+
+	void Awake() {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			Vector3 mp = Input.mousePosition;
+			mp = Camera.main.ScreenToWorldPoint (mp);
+			mp.z = 0.0f;
+			Boid b = Instantiate (boidPrefab, mp, Quaternion.identity).GetComponent<Boid>();
+			b.target = player.transform;
+			boids.Add (b);
+		}
+	}
+}
